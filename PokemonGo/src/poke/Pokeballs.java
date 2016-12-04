7180f6113a778ec12b4826ab62adcf0feff2b362
my3vshenoy <vincent-macbook@Vincents-MacBook-Pro.local>
2016-12-03 20:27:05 -0500

    fixing errors after testing 2 of 2

---
+++ PokemonGo/src/poke/Pokeballs.java
@@ -0,0 +1,74 @@
+package poke;
+import java.util.Scanner;
+
+public class Pokeballs 
+{
+	private String[] pokeballArray = {"", "Pokeball", "Greatball", "Ultraball"};
+	private String pokeball = "";
+	private int[] quantityBallArray = {0, 3, 3, 3};
+	private int quantityBall = 0;
+	private double[] ballModifierArray = {0, 0, 0.1, 0.2};
+	private double ballModifier = 0.0;
+	private int ballSelection = 0;
+//This needs to be moved to pokemongo
+//	private double catchRate = 0.0;
+//	private int maxHP = 100;
+
+	public Pokeballs (String pokeball, int quantityBall, double ballModifier)
+	{
+		this.pokeball = pokeball;
+		this.quantityBall = quantityBall;
+		this.ballModifier = ballModifier;
+	}
+	
+	public void setPokeball(String pokeball)
+		{this.pokeball = pokeballArray[ballSelection];}
+	public void setQuantityBall (int quantityBall)
+		{this.quantityBall = quantityBallArray[ballSelection];}
+	public void setBallModifier(double ballModifier)
+		{this.ballModifier = ballModifierArray[ballSelection];}
+	
+	public String getPokeball()
+		{return pokeball;}
+	public int getQuantityBall()
+		{return quantityBall;}
+	public double getBallModifier()
+		{return ballModifier;}
+	
+	public void usePokeballs()
+	{
+		System.out.println("Which type of pokeball do you want to use: (choose from 1~3)");
+		System.out.printf("1. Pokeball(%d)%n2. Greatball(%d)%n3. Ultraball(%d)%n", 
+			quantityBallArray[1], quantityBallArray[2], quantityBallArray[3]);
+		//accept input from the user
+		ballSelection = new Scanner(System.in).nextInt();
+		System.out.println();
+		//reduce pokeball count by 1
+		if(quantityBallArray[ballSelection]>0)
+		{
+			quantityBall = quantityBallArray[ballSelection]--;
+			pokeball = pokeballArray[ballSelection];
+			ballModifier = ballModifierArray[ballSelection];
+		}
+		else
+		{
+			quantityBall = 0;
+			pokeball = pokeballArray[ballSelection];
+			ballModifier = 0;
+		}
+	}
+}
+
+			//this needs to be moved out to pokemongo
+//			catchRate = (double) opponentPokemon.getHP() / maxHP - ballModifierArray[ballSelection]; 
+//			if(catchRate <= 0.25)
+//			{
+//				System.out.printf("You caught %s!%n", opponentPokemon.getName());
+//				//If you catch the pokemon, need way to exit battle
+//			}
+//			else
+//				System.out.printf("You did not catch %s!%n", opponentPokemon.getName());
+//		}			
+//		else
+//			System.out.printf("You are out of %ss. You lost your turn.%n%n", pokeballArray[ballSelection]);  							
+//			//use another switch statement to call the usePokeball method of the selected potion
