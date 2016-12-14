11bb157cd24cfe7b201aa53b024dee59dd580dc5
my3vshenoy <vincent-macbook@Vincents-MacBook-Pro.local>
2016-12-13 21:14:29 -0500

    amishi updates to poke store

--- PokemonGo/src/poke/PokeStore.java
+++ PokemonGo/src/poke/PokeStore.java
@@ -1,32 +1,63 @@
-//package poke;
-//import java.util.Scanner;
-//
-//import static java.lang.System.out;
-//
-//public class PokeStore {
-//	public String[] itemsForSale;{"Pokeball","Greatball","Ultraball","Potion","Super Potion","Hyper Potion"};
-//	public int[] priceList;
-//	public int listLength=itemsForSale.length;
-//	
-//	
-//	
-//	System.out.println("Hello, and welcome to the PokeStore.\n");
-//	System.out.printf("Your current cash balance is %d. %n",player.getMoney());
-//	System.out.println("What would you like to purchase today?");
-//	
-//	private int counter=0
-//	while (listLength >= counter){
-//		System.out.printf("%s   $%d %n", itemsForSale[counter],priceList[counter]);
-//		counter++;
-//	}
-//	Scanner itemSelected = new Scanner (System.in);
-//	String activitySelection = reader2.next();
-//	
-//	//For each of the six items in the itemsForSale list, create a case
-//	//which adds that item to the Player's list
-//	switch(itemSelected){
-//		case 1:
-//			
-//
-//}}
-//}
+/*package poke;
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+public class PokeStore {
+	public String[] itemsForSale = {"Pokeball","Greatball","Ultraball","Potion","Super Potion","Hyper Potion"};
+	public int[] priceList = { 50, 75, 100, 75, 100, 140 };
+	public int listLength=itemsForSale.length;
+	
+	System.out.println("Hello, and welcome to the PokeStore.\n");
+	System.out.printf("Your current cash balance is %d. %n",player.getMoney());
+	System.out.println("What would you like to purchase today?");
+	
+	private int counter=0
+	while (listLength >= counter){
+		System.out.printf("%s   $%d %n", itemsForSale[counter],priceList[counter]);
+		counter++;
+	}
+	Scanner itemSelected = new Scanner (System.in);
+	String activitySelection = reader2.next();
+	
+	//For each of the six items in the itemsForSale list, create a case
+	//which adds that item to the Player's list
+	switch(itemSelected){
+		case 1:
+			{ if (itemSelected = itemsForSale[0]);
+			money = money - priceList[0];			
+			pokeball++;
+			break; }
+		case 2:
+				if (itemSelected = itemsForSale[1]);
+				{money = money - priceList[1];
+				pokeball++;
+				break; 	
+		case 3: 
+				if (itemSelected = itemsForSale[3]);
+				{money = money - priceList[0];
+				pokeball++;
+				break;
+				
+		case 4: 
+			    if (itemSelected = itemsForSale[4]);
+				{money = money - priceList[4];
+				potions++;
+				break;	
+				
+		case 5:
+			    if (itemSelected = itemsForSale[5]);
+			   money = money - priceList[5];
+			   potions++;
+			   break;	
+		case 6:
+		    if (itemSelected = itemsForSale[6]);
+		   money = money - priceList[6];
+		   potions++;
+		   break;
+		   
+		default: System.out.println("That is not a valid selection from the PokeStore.");
+
+}}
+}
+*/
